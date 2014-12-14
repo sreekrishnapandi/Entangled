d06c8ee5743d81ee08a2284eaaef7444216eae6d
Krish <sreekrishnapandi@gmail.com>
2014-12-14 19:46:14 +0530

    #Averaged Statistics -

--- Multipath_Testbed.py
+++ Multipath_Testbed.py
@@ -7,25 +7,57 @@ from math import sqrt
 import threading
 import time
 
-
 global DECODED
 global buf
 global adr
 global relays
 global sleep_time
+global time_taken
+global encoded_packets
+global decoded_packets
+global lin_dep_pkts
+global relayed_pkts
 
 relays = 3
 sleep_time = 0.00001
 
-
 DECODED = False
 buf = [0 for _ in range(20)]
 adr = [(1000, 1000) for _ in range(20)]
+time_taken = 0
+encoded_packets = 0
+decoded_packets = 0
+lin_dep_pkts = 0
+relayed_pkts = 0
+
+
+def initialize():
+    global DECODED
+    global buf
+    global adr
+    global relays
+    global sleep_time
+    global time_taken
+    global encoded_packets
+    global decoded_packets
+    global lin_dep_pkts
+    global relayed_pkts
+    DECODED = False
+    buf = [0 for _ in range(20)]
+    adr = [(1000, 1000) for _ in range(20)]
+    time_taken = 0
+    encoded_packets = 0
+    decoded_packets = 0
+    lin_dep_pkts = 0
+    relayed_pkts = 0
 
 
 # def dist(self, x, y):
 #     print(sqrt((self.x - x)**2 + (self.y - y)**2))
 #     return sqrt((self.x - x)**2 + (self.y - y)**2)
+
+
+def delay(): time.sleep(0.001)
 
 
 def threaded(fn):
@@ -40,6 +72,8 @@ class Node:
         self.y = y
         self.bufindex = bufindex
         adr[bufindex] = (x, y)
+        self.symbol_size = 128
+        self.symbols = 60
 
     def dist(self, x, y):
         # print(x)
@@ -47,20 +81,17 @@ class Node:
         # print(self.x)
         # print(self.y)
         distance = sqrt((self.x - x)**2 + (self.y - y)**2)
-        print("Distance = " + str(distance))
+        #print("Distance = " + str(distance))
         return distance
 
     @threaded
     def source(self, FILE):
+        global encoded_packets
         # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # s.bind(('', 8500))
 
-
-        symbol_size = 128
-        symbols = 60
-
-        encoder_factory = kodo.FullVectorEncoderFactoryBinary(symbols, symbol_size)
+        encoder_factory = kodo.FullVectorEncoderFactoryBinary(self.symbols, self.symbol_size)
         encoder = encoder_factory.build()
 
         if not FILE == "":
@@ -76,27 +107,26 @@ class Node:
         """
         #print(self.bufindex)
         while not DECODED:
-            print("Encoding...")
+            #print("---Encoding---")
             buf[self.bufindex] = encoder.encode()
+            encoded_packets += 1
             time.sleep(sleep_time)                    # DELAY INTRODUCED to synchronise Encoding and decoding.
 
     @threaded
     def relay(self):
         global DECODED
         # print(".........Decoding..")
-        symbol_size = 128
-        symbols = 60
 
-        decoder_factory = kodo.FullVectorDecoderFactoryBinary(symbols, symbol_size)
+        decoder_factory = kodo.FullVectorDecoderFactoryBinary(self.symbols, self.symbol_size)
         recoder = decoder_factory.build()
 
-        print(adr[1])
+        #print(adr[1])
 
         while not DECODED:
-            print("####Relay#### " + str(self.bufindex))
+            #print("####Relay#### " + str(self.bufindex))
             for i in range(relays-1):
                 #sqrt((self.x - adr[i][0])**2 + (self.y - adr[i][1]))**2)
-                if np.random.randint(100) >= (self.dist(adr[i][0], adr[i][1])) * 5 and not adr[i] == (self.x, self.y):
+                if np.random.randint(100) >= (self.dist(adr[i][0], adr[i][1])) * 5 and not adr[i] == (self.x, self.y) and not buf[i] == 0:
 #                if np.random.randint(100) >= (sqrt((self.x - adr[i][0])**2 + (self.y - adr[i][1])**2)) * 5 and not adr[i] == (self.x, self.y):
                     recoder.decode(buf[i])
                     buf[self.bufindex] = recoder.recode()
@@ -106,38 +136,84 @@ class Node:
 
     @threaded
     def sink(self):
+        start = time.time()
         global DECODED
+        global time_taken
+        global decoded_packets
         # print(".........Decoding..")
-        symbol_size = 128
-        symbols = 60
 
-        decoder_factory = kodo.FullVectorDecoderFactoryBinary(symbols, symbol_size)
+        decoder_factory = kodo.FullVectorDecoderFactoryBinary(self.symbols, self.symbol_size)
         decoder = decoder_factory.build()
 
-        print(adr[1])
 
         while not decoder.is_complete():
             for i in range(relays):
-                print("##DECODER##")
+                #print("##DECODER##")
                 #sqrt((self.x - adr[i][0])**2 + (self.y - adr[i][1]))**2)
-                if np.random.randint(100) >= (self.dist(adr[i][0], adr[i][1])) * 5 and not adr[i] == (self.x, self.y):
+                if np.random.randint(100) >= (self.dist(adr[i][0], adr[i][1])) * 5 and not adr[i] == (self.x, self.y) and not buf[i] == 0:
 #                if np.random.randint(100) >= (sqrt((self.x - adr[i][0])**2 + (self.y - adr[i][1])**2)) * 5 and not adr[i] == (self.x, self.y):
                     decoder.decode(buf[i])
-                    print("Decoder Rank : " + str(decoder.rank()))
+                    decoded_packets += 1
+                    #print("Decoder Rank : " + str(decoder.rank()))
             time.sleep(sleep_time)              # DELAY INTRODUCED to synchronise Encoding and decoding.
 
         if decoder.is_complete():
             DECODED = True
-            print("DECODED")
+            #print("DECODED")
+            time_taken = time.time() - start
+
+# src = Node(0, 0, 0)
+# relay = Node(0, 20, 1)
+# snk = Node(0, 10, 2)
+#
+#
+# src.source("")
+# delay()
+# relay.relay()
+# delay()
+# snk.sink()
+# while not DECODED: pass
+#
+# delay()
+# print("\nTime Taken    : " + str(time_taken))
+# print("Min req packets : " + str(src.symbols))
+# print("Encoded Pkts : " + str(encoded_packets))
+# print("Decoded Pkts : " + str(decoded_packets))
 
 
-src = Node(0, 0, 0)
-relay = Node(0, 2, 1)
-snk = Node(0, 5, 2)
+"""------- Average Statistics (Text report) --------"""
+
+# src = Node(0, 0, 0)
+# relay = Node(0, 20, 1)
+# snk = Node(0, 10, 2)
+
+avg_time_taken = 0
+avg_encoded_packets = 0
+avg_decoded_packets = 0
+
+iterations = 1000
+
+for i in range(iterations):
+    initialize()
+    src = Node(0, 0, 0)
+    relay = Node(0, 20, 1)
+    snk = Node(0, 10, 2)
+
+    src.source("")
+    delay()
+    relay.relay()
+    delay()
+    snk.sink()
+    while not DECODED: pass
+    print("================------------------------- Encoded Pkts : " + str(encoded_packets))
+    avg_time_taken += time_taken
+    avg_encoded_packets += encoded_packets
+    avg_decoded_packets += decoded_packets
 
 
-src.source("")
-time.sleep(0.001)
-relay.relay()
-time.sleep(0.001)
-snk.sink()
+
+delay()
+print("\nTime Taken    : " + str(avg_time_taken/iterations))
+print("Min req packets : " + str(src.symbols))
+print("Encoded Pkts : " + str(avg_encoded_packets/iterations))
+print("Decoded Pkts : " + str(avg_decoded_packets/iterations))
